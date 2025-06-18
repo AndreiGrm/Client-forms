d89ecd1f2ef8d077cba9f5632a255d04103f6d98
andrei.gramada <andrei.gramada@timeneye.com>
2025-06-18 10:52:38 +0200

    feat: added empty login page

---
+++ src/app/presentation/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-residence',
+  imports: [
+  ],
+  templateUrl: './residence.component.html',
+  styleUrl: './residence.component.css'
+})
+export default class LoginComponent {
+  
+}
